ff9e5ef75c15fbcb5657a3b9cd82207c84478b86
ccchitiva <61605993+ccchitiva@users.noreply.github.com>
2020-03-11 20:51:43 -0500

    Update Usuarios.php

--- src/routes/Usuarios.php
+++ src/routes/Usuarios.php
@@ -39,7 +39,7 @@ $app->get('/api/Usuarios', function(Request $request, Response $response){
 $app->get('/api/Usuarios/{ID}', function(Request $request, Response $response){
     $ID = $request->getAttribute('ID');
 
-    $sql = "SELECT * FROM Usuarios WHERE ID = $2";
+    $sql = "SELECT * FROM Usuarios WHERE ID = '2'";
 
     try{
         // Get DB Object
